8f3b53251c7226fcaa82a616e9a6f31b4a2ee525
unknown <sjjoseph106@gmail.com>
2022-05-27 23:14:54 +0900

    Chain

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,11 @@ interface BlockShape {
   data: string;
 }
 class Block implements BlockShape {
-  public hash: string;
+  public readonly hash: string;
   constructor(
-    public prevHash: string,
-    public height: number,
-    public data: string
+    public readonly prevHash: string,
+    public readonly height: number,
+    public readonly data: string
   ) {
     this.hash = Block.calculateHash(prevHash, height, data);
   }
@@ -20,3 +20,33 @@ class Block implements BlockShape {
     return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks(): readonly Block[] {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+
+console.log(blockchain.getBlocks());
